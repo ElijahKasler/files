4485dccf0abd7c0cbfbb4fa46481fa58852afe94
ElijahKasler <elijahkasler24@gmail.com>
2025-05-24 09:16:17 -0400

    Update expliot.js

--- expliot.js
+++ expliot.js
@@ -1,6 +1,6 @@
 /// execute_script.js
 window.addEventListener("keydown", event => {
-  if (event.code === "ShiftRight" && !event.repeat && !hasRun) {
+  if (event.code === "ShiftRight" ) {
     let code = `alert("hi")`;
     eval(code);
   }
