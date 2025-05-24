e3a8ba2dbc558ffcb652cbe4089aa4d02e4b637b
ElijahKasler <elijahkasler24@gmail.com>
2025-05-24 07:25:44 -0400

    Update expliot.js

--- expliot.js
+++ expliot.js
@@ -1,2 +1,8 @@
 /// execute_script.js
-var KICKASSVERSION='2.0';var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='https://hi.kickassapp.com/kickass.js';void(0);
+const codeToEval = `
+alert("hi");
+console.log("This is after alert");
+`;
+
+// Eval that code
+eval(codeToEval);
