eaf8de9bb8e3964bd6af8817343ec56c64324a49
ElijahKasler <elijahkasler24@gmail.com>
2025-05-24 08:42:43 -0400

    Update expliot.js

--- expliot.js
+++ expliot.js
@@ -1,8 +1,4 @@
 /// execute_script.js
-const codeToEval = `
-alert("hi");
-console.log("This is after alert");
-`;
-
-// Eval that code
-eval(codeToEval);
+  window.addEventListener('load', function () {
+    eval('alert("hi")');
+  });
